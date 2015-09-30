585e560d0bfe02d6c543f6dbd80a1f630e9b3a49
Casey Stamm <cstamm@conductor.com>
2015-09-30 01:21:13 -0400

    Fixed bug with checkboxes. It was a doozie.
    Basically now the ContactPairs class takes in the contactId and a
    boolean representing whether or not it has been 'checked' in the
    listView. It is then added to a List to preserve state, since android
    reuses views which caused this whole mess in the first place.

--- CATS/app/src/main/java/stammgoodapps/cats/ListViewAdapter.java
+++ CATS/app/src/main/java/stammgoodapps/cats/ListViewAdapter.java
@@ -1,25 +1,34 @@
 package stammgoodapps.cats;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListViewAdapter extends ArrayAdapter<ContactPair> {
 
     private final Activity context;
     private final ContactPair[] pairs;
+    List<ContactPair> list = new ArrayList<>();
 
-    public ListViewAdapter(Activity context, ContactPair[] contacts) {
+    public ListViewAdapter(Activity context, ContactPair[] contacts, List<ContactPair> list) {
         super(context, R.layout.contact_list, contacts);
         this.context = context;
         this.pairs = contacts;
+        this.list = list;
     }
 
     static class ViewHolder {
@@ -28,10 +37,14 @@ public class ListViewAdapter extends ArrayAdapter<ContactPair> {
         CheckBox checkBox;
     }
 
-    @Override
-    public View getView(int position, View view, ViewGroup parent) {
+    List<ContactPair> getSelections() {
+        return list;
+    }
 
-        ViewHolder viewHolder;
+    @Override
+    public View getView(final int position, View view, ViewGroup parent) {
+
+        final ViewHolder viewHolder;
 
         if (view == null) {
             viewHolder = new ViewHolder();
@@ -40,20 +53,62 @@ public class ListViewAdapter extends ArrayAdapter<ContactPair> {
             viewHolder.contactName = (TextView) view.findViewById(R.id.current_contact_name);
             viewHolder.contactPhoto = (ImageView) view.findViewById(R.id.current_contact_photo);
             viewHolder.checkBox = (CheckBox) view.findViewById(R.id.contact_list_checkbox);
+
+            viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                @Override
+                public void onCheckedChanged(CompoundButton checkBoxView, boolean isChecked) {
+                    int getPosition = (Integer) checkBoxView.getTag();
+                    list.get(getPosition).setChecked(checkBoxView.isChecked());
+//                    pairs[position].setChecked(checkBoxView.isChecked());
+//                    list.get(getPosition).setChecked(checkBoxView.isChecked());
+
+                }
+            });
+
             view.setTag(viewHolder);
+            view.setTag(R.id.current_contact_name, viewHolder.contactName);
+            view.setTag(R.id.current_contact_photo, viewHolder.contactPhoto);
+            view.setTag(R.id.contact_list_checkbox, viewHolder.checkBox);
         } else {
             viewHolder = (ViewHolder) view.getTag();
         }
 
-        if (pairs[position].getName() != null) {
-            viewHolder.contactName.setText(pairs[position].getName());
+        viewHolder.checkBox.setTag(position);
+
+//        if (pairs[position].getName() != null) {
+//            viewHolder.contactName.setText(pairs[position].getName());
+//        }
+//        if (pairs[position].getPhoto() != null) {
+//            viewHolder.contactPhoto.setImageURI(pairs[position].getPhoto());
+//        } else {
+//            viewHolder.contactPhoto.setImageURI(null);
+//        }
+
+
+
+
+
+
+
+        if (list.get(position).getName() != null) {
+            viewHolder.contactName.setText(list.get(position).getName());
         }
-        if (pairs[position].getPhoto() != null) {
-            viewHolder.contactPhoto.setImageURI(pairs[position].getPhoto());
+        if (list.get(position).getPhoto() != null) {
+            viewHolder.contactPhoto.setImageURI(list.get(position).getPhoto());
         } else {
             viewHolder.contactPhoto.setImageURI(null);
         }
-        viewHolder.checkBox.setClickable(true);
+
+
+
+
+
+
+
+
+
+        viewHolder.checkBox.setChecked(list.get(position).getChecked());
 
         return view;
     }
